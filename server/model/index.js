61fad33bc097a6ad9bc39fac914f5f1f5153f390
FauzanAr <faurinta12345@gmail.com>
2022-05-21 13:37:32 +0700

    Feature: Article - create schema for comment and article

---
+++ server/model/index.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const ArticleSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    comments: {
+        type: [CommentSchema],
+    },
+    createdAt: {
+        type: Number,
+        required: true,
+    },
+    updatedAt: {
+        type: Number,
+        required: true,
+    },
+});
+
+const CommentSchema = new Schema({
+    description: {
+        type: String,
+        required: true,
+    },
+    createdAt: {
+        type: Number,
+        required: true,
+    },
+    updatedAt: {
+        type: Number,
+        required: true,
+    },
+});
+
+const Article = mongoose.model('Articles', ArticleSchema);
+
+module.exports = {
+    Article
+}
