deba8fda54b2eff4ad3d6fe1910d9488b022dfd4
FauzanAr <faurinta12345@gmail.com>
2022-05-21 12:22:53 +0700

    Feature: Articles - set up mongodb connection

--- server/config/index.js
+++ server/config/index.js
@@ -1,7 +1,11 @@
 require('dotenv').config();
 
 const config = {
-    port: process.env.PORT
+    port: process.env.PORT,
+    mongoDb: {
+        url: process.env.MONGODB_URL,
+        collectionName: process.env.COLLECTION_NAME,
+    }
 }
 
 module.exports = config;
--- server/server.js
+++ server/server.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 const { ApolloServer, gql } = require('apollo-server-express');
 
 const config = require('./config')
 const typeDefs = require('./graphql/schema');
 const resolvers = require('./graphql/resolver');
 
-const server = new ApolloServer({typeDefs, resolvers})
+const port = config.port || 5000;
+const server = new ApolloServer({
+    typeDefs, 
+    resolvers,
+    playground: {
+        endpoint: `http://localhost:${port}/graphql`,
+        settings: {
+            'editor.theme': 'light'
+        }
+    }
+});
 const app = express();
 
 app.use(cors());
@@ -15,9 +26,15 @@ app.get('/', (req, res) => {
     res.send('Server running properly')
 });
 
-server.applyMiddleware({ app })
+server.applyMiddleware({ app });
 
-const port = config.port || 5000
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`)
-})
+mongoose
+    .connect(config.mongoDb.url, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then((res) => {
+        app.listen(port, () => {
+            console.log(`Server running on port ${port}`)
+        })
+    })
+    .catch((err) => {
+        console.error('Error while connect to db', err)
+    })
